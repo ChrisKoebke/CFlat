a20a8376fb65f17812a126596856b89b9037f961
Christoph Köbke <c.koebke@hotmail.de>
2020-04-08 21:55:31 +0200

    Moved NodeWithToken to Syntax/NodeTokenPair.

--- Syntax/NodeTokenPair.cs
+++ Syntax/NodeTokenPair.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CFlat
+namespace CFlat.Syntax
 {
-    public class NodeWithTokenType
+    public class NodeTokenPair
     {
         public INode Node;
         public TokenType Type;
